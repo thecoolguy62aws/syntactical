f98db6545cbe2b2b96901ca48d0f1c239abe75ea
thecoolguy62aws <150736455+thecoolguy62aws@users.noreply.github.com>
2026-03-13 18:06:24 -0700

    docs(comments): fix typo of the word "grammar"

--- src/syntactical/syntactical.py
+++ src/syntactical/syntactical.py
@@ -4,7 +4,7 @@ from lark import Lark, Transformer, v_args
 import os
 from syntactical import version # This imports the __version__ variable from version.py
 
-# This is the main gramer of the language:
+# This is the main grammar of the language:
 grammar = r"""
     start: line_content+
     ?line_content: statement (SEMICOLON statement)* [SEMICOLON]
