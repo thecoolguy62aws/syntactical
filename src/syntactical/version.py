5be8f6d1e1ff1dae9a616be68ee3ab037c94f469
thecoolguy62aws <150736455+thecoolguy62aws@users.noreply.github.com>
2026-03-14 10:39:36 -0700

    docs(comments): add comments to version.py file

--- src/syntactical/version.py
+++ src/syntactical/version.py
@@ -1,6 +1,9 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
+    # Find the version of Syntactical and put it in the __version__ variable that can be externally imported from the main file:
     __version__ = version("syntactical")
+
+# If Syntactical is not found (this should never happen; you are using Syntactical) then just put "unknown" as the version:
 except PackageNotFoundError:
     __version__ = "unknown"
